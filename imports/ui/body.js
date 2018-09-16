9aac1f2cdc3ef0566b0d2101882ab149898c18b3
snetts <snettsblog@snetts.com>
2018-09-16 09:32:06 +0000

    First commit

---
+++ imports/ui/body.js
@@ -0,0 +1,15 @@
+import {
+    Meteor
+} from "meteor/meteor";
+import {
+    Template
+} from 'meteor/templating';
+import {
+    ReactiveDict
+} from 'meteor/reactive-dict';
+import {
+    Tasks
+} from '../api/eits.js';
+
+import './eits.js';
+import './body.html';
