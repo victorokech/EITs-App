8ebb4a01c3bed3306672e0578046ccc8e8753bae
snetts <snettsblog@snetts.com>
2018-09-18 09:38:28 +0000

    Build edit feature

--- imports/api/eits.js
+++ imports/api/eits.js
@@ -4,13 +4,38 @@ import {
 import {
     Mongo
 } from 'meteor/mongo';
+import {
+    check
+} from 'meteor/check';
+
 
 export const EITS = new Mongo.Collection('eits');
 
 Meteor.methods({
     'eit.insert'(eits) {
         EITS.insert({
-            eits
+            firstname: eits.firstname,
+            lastname: eits.lastname,
+            gender: eits.gender,
+            dob: eits.dob
         });
-    }
+    },
+    'eit.update'(id, eits) {
+        check(id, String);
+        EITS.update(id, eits);
+    },
+    'eit.remove'(id) {
+        check(id, String);
+        EITS.remove(id);
+    },
+    'eit.setChecked'(taskId, setChecked) {
+        check(taskId, String);
+        check(setChecked, Boolean);
+
+        Tasks.update(taskId, {
+            $set: {
+                checked: setChecked
+            }
+        });
+    },
 });
--- imports/ui/body.js
+++ imports/ui/body.js
@@ -21,18 +21,49 @@ Template.body.onCreated(function bodyOnCreated() {
 Template.body.helpers({
     eits() {
         return EITS.find({});
-    }
+    },
 });
 
 Template.body.events({
     'submit .new-eit'(event) {
-       // get eit data from form and save
+        event.preventDefault();
+        // get eit data from form and save
         const target = event.target;
-        var eits = { firstname: target.firstname.value, lastname: target.lastname.value, gender: target.gender.value, dob: target.dob.value };
 
-        Meteor.call('eit.insert', eits);
+        var id = target.id.value;
 
+        console.log(id);
+
+        var eits = {
+            firstname: target.firstname.value,
+            lastname: target.lastname.value,
+            gender: target.gender.value,
+            dob: target.dob.value
+        };
+
+        if (!id) {
+            Meteor.call('eit.insert', eits);
+        } else {
+            Meteor.call('eit.update', id, eits);
+        }
         // clear form
-        target.eits.value = '';
-    }
+        target.reset();
+    },
+    'click .btn-edit'() {
+        const edit = document.querySelector('.new-eit');
+
+        // getting eit to edit
+        edit.firstname.value = this.firstname;
+        edit.lastname.value = this.lastname;
+        edit.gender.value = this.gender;
+        edit.dob.value = this.dob;
+        edit.id.value = this._id;
+
+
+        edit.submit.innerHTML = "Update";
+
+    },
+    'click .btn-delete'() {
+        Meteor.call('eit.remove', this._id);
+    },
 });
