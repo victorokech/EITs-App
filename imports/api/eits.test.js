4f5dd7a353285d7ab264572708225a91987f9fe8
Victor Okech <vickesnet@gmail.com>
2019-01-29 09:56:24 +0000

    Adding testing v2

---
+++ imports/api/eits.test.js
@@ -0,0 +1,11 @@
+/* eslint-env mocha */
+
+import { Meteor } from 'meteor/meteor';
+
+if (Meteor.isServer) {
+    describe('EITS', () => {
+        describe('methods', () => {
+            it('can delete owned task', () => {});
+        });
+    });
+}
